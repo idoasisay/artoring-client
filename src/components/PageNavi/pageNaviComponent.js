f5b24201971721262de283478717101433ec71d0
zpxlffjrm <scm0222@naver.com>
2021-07-11 12:22:13 +0900

    component test finished

--- src/components/PageNavi/pageNaviComponent.js
+++ src/components/PageNavi/pageNaviComponent.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Routes from './routes';
+import Footer from './footer';
 import '../../css/PageNavi.css';
 
 const Nav = (props) => {
   return (
     <nav className='navbar'>
       <Routes />
+      <Footer />
     </nav>
   );
 };
