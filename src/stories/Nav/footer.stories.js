203cf5e278d36a6703868a8ed460f34fed908faf
zpxlffjrm <scm0222@naver.com>
2021-07-12 11:10:43 +0900

    update package.json

--- src/stories/Nav/footer.stories.js
+++
@@ -1,8 +0,0 @@
-import Footer from '../../components/Nav/Footer';
-
-export default {
-  title: 'artoring/Navigation/Footer',
-  component: Footer
-};
-
-export const Template = (args) => <Footer {...args} />;
