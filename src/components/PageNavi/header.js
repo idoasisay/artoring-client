888f0ce6f7cf1e2f8154f50a662fa0831259eb34
zpxlffjrm <scm0222@naver.com>
2021-08-05 18:39:07 +0900

    fix broken structure

--- src/components/PageNavi/header.js
+++ src/components/PageNavi/header.js
@@ -83,18 +83,18 @@ function Header ({ isLogin, profile, searchDataHandler }) {
                 onMouseOver={() => classReplacer('.isLogin', 'isLogin ClickOrMouseUp')}
                 onMouseLeave={() => classReplacer('.isLogin', 'isLogin')}
               >로그인/회원가입
-            </Link>}
+              </Link>}
 
         </div>
       </div>
-    </div>
+      </div>
     : <div className='nav Flex'>
       <img src={process.env.PUBLIC_URL + '/img/search.svg'} className='SearchIcon' alt='검색' />
       <form onSubmit={handleSubmit(handleSearch)} className='SearchForm'>
         <input type='text' {...register('search')} placeholder='검색어를 입력해 주세요. ' className='SearchPlaceHolder TextType2' />
       </form>
       <div className='CloseSearch' onClick={() => searchingToggler(false)}>&times;</div>
-      </div>
+    </div>
   );
 }
 
