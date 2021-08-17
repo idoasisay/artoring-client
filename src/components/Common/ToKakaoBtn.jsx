f4db21ff47d28ac95ce57d40620b78be828504b8
zpxlffjrm <scm0222@naver.com>
2021-08-17 10:51:58 +0900

    update comments

--- src/components/Common/ToKakaoBtn.jsx
+++ src/components/Common/ToKakaoBtn.jsx
@@ -1,13 +1,6 @@
 import { useEffect } from 'react';
 
 const Kakao1by1Btn = () => {
-  // useEffect(() => {
-  //   (window).Kakao.Channel.createAddChannelButton({
-  //     container: '#kakao-add-channel-button',
-  //     channelPublicId: '_jmjqs' // 카카오톡 채널 홈 URL에 명시된 id로 설정합니다.
-  //   });
-  // }, []);
-
   const clickHandler = () => {
     window.Kakao.Channel.chat({
       channelPublicId: '_jmjqs' // 카카오톡 채널 홈 URL에 명시된 id로 설정합니다.
